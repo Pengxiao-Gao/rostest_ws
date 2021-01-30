4429fb0a6ee8c1edd0fd4024cdbf845a5a1baa8b
StefanGao1114 <504207850@qq.com>
2021-01-30 10:10:49 +0100

    service

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,4 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export PWD='/home/stefan/rostest/build'
+export PWD='/home/stefan/rostest_ws/build'
--- build/learning_topic/CMakeFiles/person_subscriber.dir/cmake_clean.cmake
+++ build/learning_topic/CMakeFiles/person_subscriber.dir/cmake_clean.cmake
@@ -1,5 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/person_subscriber.dir/src/person_subscriber.cpp.o"
+  "/home/stefan/rostest_ws/devel/lib/learning_topic/person_subscriber.pdb"
+  "/home/stefan/rostest_ws/devel/lib/learning_topic/person_subscriber"
+)
 
 # Per-language clean rules from dependency scanning.
-foreach(lang )
+foreach(lang CXX)
   include(CMakeFiles/person_subscriber.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
